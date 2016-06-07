b42265c450969aed7a8fe44bfb48aea1570a966e
Kel Kanhirun <kel.kanhirun@gmail.com>
2016-06-07 10:15:38 -0500

    Config: sets Karma reporter to `docs` (default)
    This provides better test feedback on readability, and allows using
    Karma's --grep option easily.

--- karma-src.conf.js
+++ karma-src.conf.js
@@ -44,7 +44,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'spec'],
+    reporters: ['spec'],
 
 
     // web server port
